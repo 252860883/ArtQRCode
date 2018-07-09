a064e9102fc55705a330512ee749f0a46548453c
honghui <252860883@qq.com>
2018-07-09 22:13:57 +0800

    made code

--- server/made.js
+++ server/made.js
@@ -3,12 +3,12 @@ let express = require('express'),
     app = new express(),
     fs = require('fs'),
     gm = require('gm'),
-    UUID=require('uuid');
+    UUID = require('uuid');
+// 转二进制
+let binaryCode = require('./toBinary').getBinary('www.baidu.com')
+// console.log(binaryCode)
 
-let toBinary =require('./toBinary')
-// 生成二维码的布尔值
-// console.log(toBinary.getBinary('666'))
- 
+// 生成二维码
 function computedImg() {
     let a = gm('./assets/avator.jpg')
         .font('./font/font.otf')    //设置字体
@@ -18,10 +18,17 @@ function computedImg() {
         .borderColor('pink')
         .border(10, 10) //边框
         .fill('#ffffff')
-        .drawRectangle(260, 260, 300, 360) //画矩形
-
-    for (let i = 0; i < 10; i++) {
-        a.draw(`image over ${i * 80},${i * 40},50,50 "./assets/wechat.png"`) //叠加图片
+        .drawRectangle(0,0,200,200)
+    for (let i = 0; i < binaryCode.length; i++) {
+        for(let j=0;j<binaryCode[i].length;j++){
+            console.log(binaryCode[i][j]) 
+            if(binaryCode[i][j]){
+                a.draw(`image over ${i*5},${j * 5},5,5 "./assets/black.jpg"`) //叠加图片
+                // a.fill('#000')
+                // a.drawRectangle(`${i*50},${j * 50},50,50`) //画矩形
+            }
+        }
+       
     }
     a.write(`./assets/output/avator${UUID.v1()}.jpg`, function (e) { //设置修改好的路径，UUID.v1()基于时间戳生成唯一标示符
         if (e) {
