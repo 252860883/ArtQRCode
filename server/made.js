8aa0a8f20582708f3266b6c54c647bb586b6fe9f
honghui <252860883@qq.com>
2018-07-09 10:57:27 +0800

    修改生成唯一标识符算法

--- server/made.js
+++ server/made.js
@@ -2,20 +2,20 @@
 let express = require('express'),
     app = new express(),
     fs = require('fs'),
-    gm = require('gm');
+    gm = require('gm'),
+    UUID=require('uuid');
 
 let toBinary =require('./toBinary')
-// let toBinary2 =require('./toBinary2')
-
-console.log(toBinary.getBinary('666'))
-
+// 生成二维码的布尔值
+// console.log(toBinary.getBinary('666'))
+ 
 function computedImg() {
     let a = gm('./assets/avator.jpg')
         .font('./font/font.otf')    //设置字体
         .fontSize(20)
         .drawText(50, 150, 'get my love') //设置水印
         .rotate('purple', 5) //旋转
-        .borderColor('pink') //
+        .borderColor('pink')
         .border(10, 10) //边框
         .fill('#ffffff')
         .drawRectangle(260, 260, 300, 360) //画矩形
@@ -23,13 +23,13 @@ function computedImg() {
     for (let i = 0; i < 10; i++) {
         a.draw(`image over ${i * 80},${i * 40},50,50 "./assets/wechat.png"`) //叠加图片
     }
-    // a.write(`./assets/output/avator${new Date().getTime()}.jpg`, function (e) { //设置修改好的路径
-    //     if (e) {
-    //         console.log(e.message)
-    //     } else {
-    //         console.log('done')
-    //     }
-    // });
+    a.write(`./assets/output/avator${UUID.v1()}.jpg`, function (e) { //设置修改好的路径，UUID.v1()基于时间戳生成唯一标示符
+        if (e) {
+            console.log(e.message)
+        } else {
+            console.log('new Image done')
+        }
+    });
 }
 
 computedImg()
